8153a3130a64453b520cd7496bec0707bed5f707
Richard Deeming <RichardD2@users.noreply.github.com>
2023-09-21 10:00:04 +0100

    Add EF7 tests; update EF Core reference version range.

---
+++ NBS.EntityFrameworkCore.SqlServer.TryParse.Tests-EF7/IntegrationTestBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NBS.EntityFrameworkCore.SqlServer.Tests
+{
+	public abstract class IntegrationTestBase
+	{
+		protected TestDbContext? DbContext { get; private set; }
+
+		[TestInitialize]
+		public void SetUp()
+		{
+			var dbContextFactory = new TestDbContextFactory();
+			DbContext = dbContextFactory.CreateDbContext(Array.Empty<string>());
+			DbContext.Database.EnsureCreated();
+		}
+
+		[TestCleanup]
+		public void TearDown()
+		{
+			if (DbContext is not null)
+			{
+				DbContext.Database.EnsureDeleted();
+				DbContext.Dispose();
+				DbContext = null;
+			}
+		}
+	}
+}
